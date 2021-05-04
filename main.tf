ec0c259e126b8f2a3c8bb329b0e98bc15fcd5bd3
juanamor93 <51340256+juanamor@users.noreply.github.com>
2021-05-04 15:01:03 +0100

    load-balancer and autoscaling modules commit

--- main.tf
+++ main.tf
@@ -17,8 +17,24 @@ module "vpc" {
   vpc_id = module.vpc.vpc_id
 }
 # EC2 Module
-module "webservers" {
+/*module "webservers" {
     source = "./aws-modules/ec2-instances"
     vpc_id = module.vpc.vpc_id
     security_group_id = module.vpc.security_group_id
+} */
+
+
+# ALB Module
+module "load-balancer" {
+  source = "./aws-modules/load-balancer"
+  vpc_id = module.vpc.vpc_id
+  subnets = module.vpc.subnet_id
+
+}  
+
+module "autoscaling" {
+  source = "./aws-modules/autoscaling"
+  security_groups = module.vpc.security_group_id 
+  subnets = module.vpc.subnet_id
+  target_group_arn = module.load-balancer.target_group_arn
 } 
