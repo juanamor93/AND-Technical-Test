377d1566904881e9bf29f06d5c5d2bb5ac57c49a
juanamor93 <51340256+juanamor@users.noreply.github.com>
2021-05-04 19:51:27 +0100

    main.tf update

--- main.tf
+++ main.tf
@@ -16,14 +16,6 @@ module "vpc" {
   source = "./aws-modules/vpc"
   vpc_id = module.vpc.vpc_id
 }
-# EC2 Module
-/*module "webservers" {
-    source = "./aws-modules/ec2-instances"
-    vpc_id = module.vpc.vpc_id
-    security_group_id = module.vpc.security_group_id
-} */
-
-
 # ALB Module
 module "load-balancer" {
   source = "./aws-modules/load-balancer"
@@ -31,7 +23,7 @@ module "load-balancer" {
   subnets = module.vpc.subnet_id
 
 }  
-
+# Autoscaling Module
 module "autoscaling" {
   source = "./aws-modules/autoscaling"
   security_groups = module.vpc.security_group_id 
